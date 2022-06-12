f1f8c49fb56970d555184b748d9dea0c2b0399e1
Shital Siddhagavali <85546693+sid4848@users.noreply.github.com>
2022-06-12 19:07:12 +0530

    Create guess_game.py

---
+++ guess_game.py
@@ -0,0 +1,11 @@
+secret_number = 9 
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if secret_number == guess:
+        print("You win!")
+        break
+    else:
+        print("You lost")
