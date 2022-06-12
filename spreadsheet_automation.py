162d5b2e04b2bf2ff2bd5fbd76e974f4f1a74d06
Shital Siddhagavali <85546693+sid4848@users.noreply.github.com>
2022-06-12 19:20:34 +0530

    Update spreadsheet_automation.py

--- spreadsheet_automation.py
+++ spreadsheet_automation.py
@@ -1,6 +1,6 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart,Reference
-def Process_workbook(filename):
+def Process_workbook(filename):  # filename -> excet sheet on which you want to work 
     wb = xl.load_workbook(filename)
     sheet = wb['Sheet1']
 
@@ -9,8 +9,12 @@ def Process_workbook(filename):
         corrected_price = cell.value*0.9
         corrected_price_cell = sheet.cell(row,4)
         corrected_price_cell.value = corrected_price
-
-    values = Reference(sheet,min_row=2,max_row=4,min_col=4,max_col=4)
+        
+    Max_row = sheet.max_row
+    Max_col = sheet.max.col
+    Min_row = sheet.mon_row
+    Min_col = sheet.min_col
+    values = Reference(sheet, min_row=Min_row, max_row=Max_row, min_col=Min_col, max_col=Max_col)
     chart = BarChart()
     chart.add_data(values)
     sheet.add_chart(chart,'e2')
