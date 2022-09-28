8c514b9e039e4e93dabf67ed9bcfc67cf7b791fe
Shital Siddhagavali <85546693+sid4848@users.noreply.github.com>
2022-09-28 17:34:48 +0530

    Delete Spin-to-WIn directory

--- Spin-to-WIn/Javascript/Thank_you.js
+++
@@ -1,9 +0,0 @@
-let popup = document.getElementById("popup");
-
-function openPopup() {
-  popup.classList.add("open_popup");
-}
-
-function closePopup() {
-  popup.classList.remove("open_popup");
-}
